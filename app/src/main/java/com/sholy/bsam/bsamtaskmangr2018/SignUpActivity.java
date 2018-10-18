6c1abe478225f1b64e2142902e47bbfc251a80e2
user <muhammed.awad@outlook.com>
2018-10-18 09:44:11 +0300

    lhon wselet allah 3lae ON CLICK Handler

--- app/src/main/java/com/sholy/bsam/bsamtaskmangr2018/SignUpActivity.java
+++ app/src/main/java/com/sholy/bsam/bsamtaskmangr2018/SignUpActivity.java
@@ -3,6 +3,9 @@ package com.sholy.bsam.bsamtaskmangr2018;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -14,16 +17,44 @@ import com.google.firebase.auth.FirebaseUser;
 public class SignUpActivity extends AppCompatActivity {
     private FirebaseAuth auth;
     private FirebaseUser firebaseUser;
+    private EditText etFirstName,etLastName,etEmail1,etPassword1,etPhone;
+    private Button btnSave;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up2);
+        setContentView(R.layout.activity_sign_up);
+
+        etFirstName = (EditText) findViewById(R.id.etFirstName);
+        etLastName = (EditText) findViewById(R.id.etLastName);
+        etEmail1 = (EditText) findViewById(R.id.etEmail);
+        etPassword1 = (EditText) findViewById(R.id.etPassword1);
+        etPhone = (EditText) findViewById(R.id.etPhone);
+        btnSave = (Button) findViewById(R.id.btnSave);
 
         auth = FirebaseAuth.getInstance();
         firebaseUser = auth.getCurrentUser();
 
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        dataHandler();
     }
+
+    private void dataHandler() {
+        boolean isk=true;
+        String email=etEmail1.getText().toString();
+        String passw1=etPassword1.getText().toString();
+        String fname=etFirstName.getText().toString();
+        String lname=etLastName.getText().toString();
+        String phone=etPhone.getText().toString();
+    }
+
     private void creatAcount (String email , String passw)
     {
         auth.createUserWithEmailAndPassword(email, passw).addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
